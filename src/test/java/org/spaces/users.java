77a3331a3c0157bddeed8cedd999337752e3d671
Ayobami Elutade <ayobami.elutade@outlook.com>
2021-04-22 10:37:26 +0100

    added users view test case

--- src/test/java/org/spaces/users.java
+++ src/test/java/org/spaces/users.java
@@ -6,22 +6,22 @@ import spaces.signIn;
 import java.io.IOException;
 
 
-public class agentManagement extends signIn {
+public class users extends signIn {
 
     @Test
 
-        public void agentManagementTest() throws InterruptedException, IOException {
+        public void usersTest() throws InterruptedException, IOException {
 
         //Sign into the app.
         driver = signIn();
 
 
-        //confirm if the agent management elements loads.
+        //confirm if the users elements loads.
         adminLandingPage landing = new adminLandingPage(driver);
 
-        if(landing.getagentManagement().checkAgentManagment()){
+        if(landing.getusers().checkUsers()){
 
-            System.out.println("Agent management module displays successfully");
+            System.out.println("User module displays successfully");
         }
         else{
             System.out.println("Oops, something went wrong");
